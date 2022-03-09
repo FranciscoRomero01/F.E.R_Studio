fa817135502acd3ddd91954b06d85f14d0cd648a
FranciscoRomero01 <89942141+FranciscoRomero01@users.noreply.github.com>
2022-03-09 11:33:10 -0300

    Se agrego el menu hamburguesa

--- js/index.js
+++ js/index.js
@@ -1,11 +1,21 @@
-$(document).ready(function(){
+const abrirMenu = document.querySelector(".abrirMenu");
 
-	$(window).scroll(function(){
-		if( $(this).scrollTop() > 0 ){
-			$('.headboard').addClass('headboard2');
-		} else {
-			$('.headboard').removeClass('headboard2');
-		}
-	});
+abrirMenu.addEventListener('click', menuabierto);
 
-});
+function menuabierto(){
+	document.querySelector(".submenu__menu").style.height="100px";
+
+	document.querySelector(".abrirMenu").style.display="none";
+
+	document.querySelector(".cerrarMenu").style.display="inline";
+}
+
+const cerrarMenu = document.querySelector(".cerrarMenu");
+
+cerrarMenu.addEventListener('click', menucerrado);
+
+function menucerrado(){
+	document.querySelector(".submenu__menu").style.height="0px";
+	document.querySelector(".abrirMenu").style.display="inline";
+	document.querySelector(".cerrarMenu").style.display="none";
+}
